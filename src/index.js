79e8c22a55fc572ec013f1bc19810afc6ad7700e
MissLiner <miss.liner@yahoo.com>
2021-10-17 11:14:10 -0400

    Create complete-task.js

--- src/index.js
+++ src/index.js
@@ -14,12 +14,7 @@
 //MODULES:
 
 
-// 3. complete-task
-//      1. change task status to completed
-//      2. change text and formatting to show it is completed
-//      3. create completed task list 
-//      4. remove completed task from active task list
-//      5. if task is already completed, toggle back to active task list
+
 // 4. Assign priority
 
 import './style.css'; 
